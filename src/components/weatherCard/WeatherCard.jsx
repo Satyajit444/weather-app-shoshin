8dc1f959dc8f16f7ff21fa5fe214e2b5078d8b1b
satyajit444777 <satyajit.senapati@nettantra.net>
2025-01-20 02:29:12 +0530

    weatheradded

--- src/components/weatherCard/WeatherCard.jsx
+++ src/components/weatherCard/WeatherCard.jsx
@@ -5,6 +5,7 @@ function WeatherCard({ currentWeather }) {
  
   return (
     <div className="text-center mb-8">
+        <img src='/assets/weather.svg' alt="" />
       <h2 className="text-6xl font-bold"> {currentWeather?.temperature}°C</h2>
       <h3> {new Date(currentWeather?.time).toLocaleString()}</h3>
     </div>
